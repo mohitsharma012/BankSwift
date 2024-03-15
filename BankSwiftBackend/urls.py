df38c0036efe70c2b7fc81ab4e852adfcd63a1b8
mohitsharma012 <stayer.mohit@gmail.com>
2024-03-15 14:51:42 +0530

    make static folder 6

--- BankSwiftBackend/urls.py
+++ BankSwiftBackend/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('api/transfer/', views.transfer_view, name='transfer_view'),
 
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
